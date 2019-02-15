66ef7243fe725a3c712fa64129c6333f9905dde0
ペリーも恐れるペリカン <kazuyaman1413@gmail.com>
2019-02-15 16:07:44 +0900

    boku ha boby

--- app/Http/Controllers/Admin/profileController.php
+++ app/Http/Controllers/Admin/profileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class profileController extends Controller
 {
     //課題４.５
@@ -12,7 +14,11 @@ class profileController extends Controller
       return view('admin.profile.create');
     }
 
-    public function creat(){
+    public function create(Request $request){
+      $this->validate($request, Profile::$rules);
+      $profile = new Profile;
+      $form = $request->all();
+
       return redirect('admin/profile/create');
     }
 
