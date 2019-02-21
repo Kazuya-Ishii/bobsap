baa8b5b384f611fc1d569aa7e3a8802dea4bc496
ペリーも恐れるペリカン <kazuyaman1413@gmail.com>
2019-02-21 23:23:04 +0900

    gyo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <p>魚雷砲　発射</p>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
